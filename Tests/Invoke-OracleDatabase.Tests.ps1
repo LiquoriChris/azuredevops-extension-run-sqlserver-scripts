283c620e740ae637587b8623039d1bc46c968937
chrisl@epi.usf.edu <chrisl@epi.usf.edu>
2019-04-17 15:52:09 -0400

    Update Pester tests.

--- Tests/Invoke-OracleDatabase.Tests.ps1
+++ Tests/Invoke-OracleDatabase.Tests.ps1
@@ -1,5 +1,7 @@
-Describe 'Invoke-OracleDatabase' {
-    It 'Should have sqlplus installed' {
-        {sqlplus -v} |Should Not throw
+Describe 'Install-DbaTools' {
+    Context 'dbatools module test' {
+        It 'Should have dbatools module installed' {
+            {Get-InstalledModule -Name dbatools} |Should Not throw
+        }
     }
 }
